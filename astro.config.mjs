bbb19d468bbbb6a53479c3ede4b45744c9c3cd51
Punit Shinde <135614556+Punit-Shinde@users.noreply.github.com>
2026-06-30 21:33:09 +0530

    Remove Cloudflare adapter from configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,8 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://likefrost.com',
   output: 'static',
-  integrations: [
-    sitemap()
-  ],
-  adapter: cloudflare({
-    platformProxy: { enabled: true }
-  })
+  integrations: [sitemap()],
 });
